ee58e1773c8e3dea0bfd9186b5d182bf2b0ef344
Pedro Pereira <pedrovbpereira10@gmail.com>
2026-06-26 09:21:14 -0300

    Esqueci de um arquivo

---
+++ src/lib/parser/grammar/attribution.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "../../record.h"
+// #include "../semantics.h"
+#include "../semantics.h"
+
+void typeCheck_id_with_expression(id $1, Record *$3) {
+    type tempVarExpTypeCmp = checkTypeCompatibility(getVarType($1), $3->type);
+    if (tempVarExpTypeCmp != NULL) {
+        if (strcmp(tempVarExpTypeCmp, getVarType($1)) == 0) {
+            return;
+        }
+    }
+    printf("ERROR: \"%s\" has type '%s', but got '%s'\n", $1, getVarType($1),
+           $3->type);
+    exit(1);
+}
+
+void attribute_id_expression(Record **$$, id $1, Record *$3) {
+    char *temp[] = {$1, "=", $3->code, ";"};
+    checkVarScope($1);
+    typeCheck_id_with_expression($1, $3);
+    *$$ = CreateRecord(cat(temp, 4));
+}
